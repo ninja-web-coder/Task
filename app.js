91b819966ec3e0682337c87dc5c1c2b48be40827
ninja-web-coder <67574086+ninja-web-coder@users.noreply.github.com>
2021-05-04 18:48:12 +0530

    uploaded task

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const bodyParser= require('body-parser')
+const mysql= require('mysql2')
+
+const app= express()
+const port= process.env.PORT || 8000;
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
+//Mysql
+
+const pool  = mysql.createPool({
+    connectionLimit : 10,
+    host            : 'localhost',
+    user            : 'root',
+    password        : 'admin',
+    database        : 'nodejs_db',
+    port            : '3307'
+})
+
+
+
+//select all data
+app.get('/', (req, res) => {
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+        console.log('connected as id ' + connection.threadId)
+        connection.query('SELECT * from candidate_test_score', (err, rows) => {
+            connection.release() // return the connection to pool
+
+            if (!err) {
+                res.send(rows)
+            } else {
+                console.log(err)
+            }
+
+            // if(err) throw err
+            console.log('The data from candidate_test_score table are: \n', rows)
+        })
+    })
+}) 
+
+
+// Add Candidate
+app.post('', (req, res) => {
+
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+        
+        const params = req.body;
+        
+        connection.query('INSERT INTO candidate_test_score SET ?', params, (err, rows) => {
+        connection.release() // return the connection to pool
+        if (!err) {
+            res.send(` record has been added.`)
+        } else {
+            console.log(err)
+        }
+        
+       
+
+        })
+        console.log(req.body)
+    })
+});
+
+
+
+
+// Listen on environment porton 8000
+app.listen(port, () => console.log('Listen on port 8000'))
